e0b10faa80b1d04ae8eb0387cf4bd5fefb3fd4f7
manuel-julian <87827982+mansch404@users.noreply.github.com>
2025-07-06 21:22:58 +0200

    Add mobile support and bold title

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ const canvas = document.getElementById('ascii-art');
 const ctx = canvas.getContext('2d');
 
 function resizeCanvas() {
-  canvas.width = window.innerWidth * 0.6;
-  canvas.height = window.innerHeight;
+  if (window.innerWidth <= 600) {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight * 0.5;
+  } else {
+    canvas.width = window.innerWidth * 0.6;
+    canvas.height = window.innerHeight;
+  }
 }
 resizeCanvas();
 window.addEventListener('resize', resizeCanvas);
